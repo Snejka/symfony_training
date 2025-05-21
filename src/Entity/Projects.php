326a095e5063bff5ca1ea513d2ecf210d3b5976e
Snezhana Krasteva <snezhana.p.plachkova@gmail.com>
2025-05-21 22:17:32 +0200

    Creates Projects Entity

---
+++ src/Entity/Projects.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\ProjectsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProjectsRepository::class)]
+#[ApiResource]
+class Projects
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $project_name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $url = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $tech = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProjectName(): ?string
+    {
+        return $this->project_name;
+    }
+
+    public function setProjectName(string $project_name): static
+    {
+        $this->project_name = $project_name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): static
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getTech(): ?string
+    {
+        return $this->tech;
+    }
+
+    public function setTech(string $tech): static
+    {
+        $this->tech = $tech;
+
+        return $this;
+    }
+}
